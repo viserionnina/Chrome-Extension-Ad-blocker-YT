1747f2a701504a7dc0d3f49f6e6465d4e8c8acf7
songbird <viserionnina@mail.com>
2026-03-12 15:53:25 +0100

    changed selective ad selectors

--- content.js
+++ content.js
@@ -1,3 +1,24 @@
-const adElements = document.querySelectorAll('[id*="ad"], [class*="ad"]');
+//const adElements = document.querySelectorAll('[id*="ad"], [class*="ad"]'); //to big and abstarct
 //removing all the id-s from adds
-adElements.forEach(el => el.remove())
+//adElements.forEach(el => el.remove())
+
+const YT_AD_SELECTORS = [
+    'ytd-ad-slot-renderer',
+    'ytd-promoted-video-renderer', 
+    'ytd-display-ad-renderer',
+    '.ytp-ad-module',
+    '#player-ads',
+    '.ad-showing'
+];
+
+const removeAds = () => {
+    YT_AD_SELECTORS.forEach(selector => {
+        document.querySelectorAll(selector).forEach(el => el.remove());
+    });
+};
+
+removeAds();
+//start again after YT dynamicly loads
+const observer = new MutationObserver(removeAds);
+observer.observe(document.body, { childList: true, subtree: true });
+
