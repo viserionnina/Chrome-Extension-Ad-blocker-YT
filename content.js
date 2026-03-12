f6a9ff829515653e94a5bd7becae0f8bd349922d
songbird <viserionnina@mail.com>
2026-03-12 15:24:12 +0100

    removing function

--- content.js
+++ content.js
@@ -1,1 +1,3 @@
-const adElements = document.querySelectorAll('[id*=*ad')
+const adElements = document.querySelectorAll('[id*="ad"], [class*="ad"]');
+//removing all the id-s from adds
+adElements.forEach(el => el.remove())
